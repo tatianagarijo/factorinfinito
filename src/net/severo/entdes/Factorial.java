6778e4315d3dcbda318456ccd917f195f60de942
TATIANA <TATIANA@DESKTOP-CO9O96Q>
2023-02-06 21:57:12 +0100

    cambio cadena texto por expresion constante

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -10,11 +10,12 @@ public class Factorial {
 
 		int n = (int) (Math.random() * 10);
 		int result = 1;
-		System.out.println("Value of n: " + n);
+		String value="Value of n: ";
+		System.out.println(value + n);
 		while (n > 1) {
 			result = result * (n - 1);
 		}
-		System.out.println("Value of n!: " + result);
+		System.out.println(value + result);
 
 	}
 
